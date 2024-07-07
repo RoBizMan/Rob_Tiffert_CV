3cddbc73cc1af0b0afaa00e2c96a7ef12bec48b4
Robert Tiffert <robert@tiffert.eu>
2024-07-07 10:25:32 +0000

    Update Google Maps marker clustering

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,3 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-import { MarkerClusterer } from "@googlemaps/markerclusterer";
-
-// [START maps_marker_clustering]
 async function initMap() {
   // Request needed libraries.
   const { Map, InfoWindow } = await google.maps.importLibrary("maps");
@@ -13,8 +5,8 @@ async function initMap() {
     "marker",
   );
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: { lat: -28.024, lng: 140.887 },
+    zoom: 5,
+    center: { lat: 50.259899, lng: 19.021585 },
     mapId: "DEMO_MAP_ID",
   });
   const infoWindow = new google.maps.InfoWindow({
@@ -49,30 +41,8 @@ async function initMap() {
 }
 
 const locations = [
-  { lat: -31.56391, lng: 147.154312 },
-  { lat: -33.718234, lng: 150.363181 },
-  { lat: -33.727111, lng: 150.371124 },
-  { lat: -33.848588, lng: 151.209834 },
-  { lat: -33.851702, lng: 151.216968 },
-  { lat: -34.671264, lng: 150.863657 },
-  { lat: -35.304724, lng: 148.662905 },
-  { lat: -36.817685, lng: 175.699196 },
-  { lat: -36.828611, lng: 175.790222 },
-  { lat: -37.75, lng: 145.116667 },
-  { lat: -37.759859, lng: 145.128708 },
-  { lat: -37.765015, lng: 145.133858 },
-  { lat: -37.770104, lng: 145.143299 },
-  { lat: -37.7737, lng: 145.145187 },
-  { lat: -37.774785, lng: 145.137978 },
-  { lat: -37.819616, lng: 144.968119 },
-  { lat: -38.330766, lng: 144.695692 },
-  { lat: -39.927193, lng: 175.053218 },
-  { lat: -41.330162, lng: 174.865694 },
-  { lat: -42.734358, lng: 147.439506 },
-  { lat: -42.734358, lng: 147.501315 },
-  { lat: -42.735258, lng: 147.438 },
-  { lat: -43.999792, lng: 170.463352 },
+  { lat: 53.776684, lng: 20.476507 },
+  { lat: 53.34938, lng: -6.260559 },
 ];
 
 initMap();
-// [END maps_marker_clustering]
