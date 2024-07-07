e65a6fc2bf5ca2550907d6a37adb7fcbdeac96ff
Robert Tiffert <robert@tiffert.eu>
2024-07-07 13:06:55 +0000

    Link CSS style for github page only and fix typo in github JS file

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -6,7 +6,7 @@ function userInformationHTML(user) {
     </h2>
     <div class="gh-content">
     <div class="gh-avatar">
-    <a href="${user.html_url} target="_blank">
+    <a href="${user.html_url}" target="_blank">
     <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
     </a>
     </div>
@@ -14,12 +14,12 @@ function userInformationHTML(user) {
     </div>`;
 }
 
-function repoInformationHTML (repos) {
+function repoInformationHTML(repos) {
     if (repos.length == 0) {
         return `<div class="clearfix repo-list">No repos!</div>`;
     }
 
-    var listItelsHTML = repos.map(function(repo) {
+    var listItemsHTML = repos.map(function (repo) {
         return `<li>
         <a href="${repo.html_url}" target="_blank">${repo.name}</a>
         </li>`;
@@ -35,12 +35,12 @@ function repoInformationHTML (repos) {
     </div>`;
 }
 
-function fetchGitHubInformation (event) {
+function fetchGitHubInformation(event) {
     $("#gh-user-data").html("");
-    $("gh-repo-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
-    if(!username) {
+    if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
@@ -55,14 +55,18 @@ function fetchGitHubInformation (event) {
         $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(firstResponse, secondResponse) {
+        function (firstResponse, secondResponse) {
             var userData = firstResponse[0];
             var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
             $("#gh-repo-data").html(repoInformationHTML(repoData));
-        }, function(errorResponse) {
+        },
+        function (errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
